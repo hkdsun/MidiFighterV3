8a746cab3bb4640a93a33e03ef5dcf0df8e4078e
Hormoz K <hkheradm@gmail.com>
2024-06-02 11:47:55 -0700

    add soem commenht

--- midifighterv3/elements.py
+++ midifighterv3/elements.py
@@ -4,11 +4,14 @@ from functools import partial
 class Elements(ElementsBase):
     def __init__(self, *a, **k):
         super().__init__(*a, **k)
+
+        # Side buttons
         self.add_button(8, "lh_button_1", channel=3, msg_type=MIDI_CC_TYPE)
         self.add_button(10, "lh_button_2", channel=3, msg_type=MIDI_CC_TYPE)
         self.add_button(11, "rh_button_1", channel=3, msg_type=MIDI_CC_TYPE)
         self.add_button(13, "rh_button_2", channel=3, msg_type=MIDI_CC_TYPE)
 
+        # Encoder Switches
         self.add_button(0, "Mute_Button", channel=1, msg_type=MIDI_CC_TYPE)
         self.add_button(1, "Solo_Button", channel=1, msg_type=MIDI_CC_TYPE)
         self.add_button(2, "Arm_Button", channel=1, msg_type=MIDI_CC_TYPE)
@@ -18,6 +21,7 @@ class Elements(ElementsBase):
         self.add_button(6, "Metronome_Button", channel=1, msg_type=MIDI_CC_TYPE)
         self.add_button(7, "Arrangement_Record_button", channel=1, msg_type=MIDI_CC_TYPE)
 
+        # Encoder Rotaries
         self.add_encoder(0, "volume_encoder", channel=0, msg_type=MIDI_CC_TYPE, is_feedback_enabled=True)
         self.add_encoder(1, "pan_encoder", channel=0, msg_type=MIDI_CC_TYPE, is_feedback_enabled=True)
         self.add_encoder(2, "track_navigation_encoder", channel=0, map_mode=MapMode.LinearBinaryOffset, msg_type=MIDI_CC_TYPE, is_feedback_enabled=True)
@@ -27,5 +31,6 @@ class Elements(ElementsBase):
         self.add_encoder(6, "cue_volume_encoder", channel=0, msg_type=MIDI_CC_TYPE, is_feedback_enabled=True)
         self.add_encoder(7, "master_volume_encoder", channel=0, msg_type=MIDI_CC_TYPE, is_feedback_enabled=True)
 
+        # Encoder Matrix (for device controls)
         self.add_encoder_matrix([[i + 8 for i in range(8)]], "Device_Controls", is_feedback_enabled=True)
 
