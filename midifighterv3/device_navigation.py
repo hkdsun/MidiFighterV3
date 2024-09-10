662b16bb43f8af41719082cb927319b551c32dc9
Hormoz K <hkheradm@gmail.com>
2024-09-10 05:32:20 -0700

    reduce sensitivity

--- midifighterv3/device_navigation.py
+++ midifighterv3/device_navigation.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from ableton.v3.control_surface.components import DeviceNavigationComponent as DeviceNavigationComponentBase
-from ableton.v3.control_surface.controls import EncoderControl, ButtonControl
+from ableton.v3.control_surface.controls import StepEncoderControl
 
 from ableton.v3.base import listens
 from ableton.v3.base import sign
-from .track_macro import TrackMacroComponent
-from functools import partial
 import logging
 logger = logging.getLogger("HK-DEBUG")
 
 
 class DeviceNavigationComponent(DeviceNavigationComponentBase):
-    scrolling_step_delay = 0.8
+
+    def __init__(self, *a, **k):
+        super(DeviceNavigationComponent, self).__init__(*a, **k)
+        self.scroll_encoder._stepper.num_steps = 4
